980f908030b2e98c0ddb6bca5b7695a18cf73e2d
Jimi-Chhatrala <jimichhatrala8488@gmail.com>
2024-06-25 14:39:44 +0530

    Home Page Completed

---
+++ ecommerce-frontend/src/components/cart-item.tsx
@@ -0,0 +1,7 @@
+type CartItemProps = {};
+
+const CartItem = ({}: CartItemProps) => {
+  return <div>CartItem</div>;
+};
+
+export default CartItem;
---
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItem from "../components/cart-item";
+
+const cartItems = [
+  {
+    productId: "adasdasd",
+    photo: "https://m.media-amazon.com/images/I/71jG+e7roXL._SX679_.jpg",
+    name: "Macbook",
+    price: 3000,
+    quantity: 4,
+    stock: 10,
+  },
+];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18);
+const shippingCharges = 200;
+const discount = 400;
+const total = subtotal + tax + shippingCharges;
+
+const Cart = () => {
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutId);
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  return (
+    <div className="cart">
+      <main>
+        {cartItems.map((i, idx) => (
+          <CartItem key={idx} />
+        ))}
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
